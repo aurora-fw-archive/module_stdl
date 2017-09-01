1c59468a2628ac9630bfbcb6f46d9d46e3880b37
Ev1lbl0w <ricasubtil@gmail.com>
2017-09-01 13:19:24 +0100

    Updated copyrigth and links

--- include/Aurora/TLib/Endian.h
+++ include/Aurora/TLib/Endian.h
@@ -1,8 +1,8 @@
 // ┌─┐┬ ┬┬─┐┌─┐┬─┐┌─┐  ┌─┐┬─┐┌─┐┌┬┐┌─┐┬ ┬┌─┐┬─┐┬┌─ | Powerful, Scalable and Cross Platform Framework
 // ├─┤│ │├┬┘│ │├┬┘├─┤  ├┤ ├┬┘├─┤│││├┤ ││││ │├┬┘├┴┐ | @author Luís Ferreira
 // ┴ ┴└─┘┴└─└─┘┴└─┴ ┴  └  ┴└─┴ ┴┴ ┴└─┘└┴┘└─┘┴└─┴ ┴ | @license GNU Public License v3
-//  Copyright (c) 2016 - Luís Ferreira. All right reserved
-//  More information in: https://github.com/ljmf00/ (Github Page)
+//  Copyright (c) 2017 - Luís Ferreira. All right reserved
+//  More information in: https://github.com/aurora-fw/ (Github Page)
 
 #ifndef INCLUDE_H_AURORA_TLIB_ENDIAN
 #define INCLUDE_H_AURORA_TLIB_ENDIAN 1
--- src/CircularShift.cpp
+++ src/CircularShift.cpp
@@ -2,8 +2,8 @@
 // ┌─┐┬ ┬┬─┐┌─┐┬─┐┌─┐  ┌─┐┬─┐┌─┐┌┬┐┌─┐┬ ┬┌─┐┬─┐┬┌─ | Powerful, Scalable and Cross Platform Framework
 // ├─┤│ │├┬┘│ │├┬┘├─┤  ├┤ ├┬┘├─┤│││├┤ ││││ │├┬┘├┴┐ | @author Luís Ferreira
 // ┴ ┴└─┘┴└─└─┘┴└─┴ ┴  └  ┴└─┴ ┴┴ ┴└─┘└┴┘└─┘┴└─┴ ┴ | @license GNU Public License v3
-//  Copyright (c) 2016 - Luís Ferreira. All right reserved
-//  More information in: https://github.com/ljmf00/ (Github Page)
+//  Copyright (c) 2017 - Luís Ferreira. All right reserved
+//  More information in: https://github.com/aurora-fw/ (Github Page)
 
 // This file is part of the Aurora Framework. This framework is free
 // software; you can redistribute it and/or modify it under the
